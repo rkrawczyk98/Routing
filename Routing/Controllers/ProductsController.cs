a583c2a4234cadd2100b6416936e1b0c2f0cf2a4
Rafał Krawczyk <rafal.krawczyk@microsoft.wsei.edu.pl>
2023-02-07 13:09:06 +0100

    Add project files.

---
+++ Routing/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Routing.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
